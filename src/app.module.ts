d8c4985df2d396501797c24db5bb3e47ef9ebccd
tkyktkmt <78991629+tkyktkmt@users.noreply.github.com>
2023-12-10 09:50:28 +0900

    app.module修正（code first）

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from 'src/prisma.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './posts/posts.module';
+import * as path from 'path';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService, PrismaService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      // 自動生成されるスキーマのファイルを指定する
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+    }),
+    PostsModule,
+  ],
 })
 export class AppModule {}
