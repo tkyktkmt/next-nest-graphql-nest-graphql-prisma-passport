de1548b7b9a12de0390f1b18a0d6623b0f7de5bc
tkyktkmt <78991629+tkyktkmt@users.noreply.github.com>
2023-12-30 15:23:41 +0900

    認証認可モジュール実装

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,4 +1,26 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Mutation, Args, Context } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { UserModel } from 'src/users/users.model';
+import { LoginUserInput } from './login-user.input';
+import { RegisterUserInput } from './register-user.input';
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalGuard)
+  @Mutation(() => UserModel, { nullable: true })
+  async login(@Args('input') input: LoginUserInput, @Context() context) {
+    console.log('== call AuthResolver login mutation ==');
+
+    return context.req.user;
+  }
+
+  @Mutation(() => UserModel, { nullable: true })
+  async register(@Args('input') input: RegisterUserInput, @Context() context) {
+    console.log('== call AuthResolver login mutation ==');
+
+    return this.authService.registerUser(input);
+  }
+}
---
+++ src/auth/serialization.provider.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PassportSerializer } from '@nestjs/passport';
+
+import { AuthService } from './auth.service';
+import { User } from './auth.interface';
+
+@Injectable()
+export class AuthSerializer extends PassportSerializer {
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
+  serializeUser(
+    user: User,
+    done: (err: Error, user: { id: string; isAdmin: boolean }) => void,
+  ) {
+    console.log('== call serializeUser ==');
+
+    done(null, { id: user.id, isAdmin: user.isAdmin });
+  }
+
+  async deserializeUser(
+    payload: { id: string; isAdmin: boolean },
+    done: (err: Error, user: Omit<User, 'password'>) => void,
+  ) {
+    console.log('== call deserializeUser ==');
+
+    const user = await this.authService.findById(payload.id);
+    done(null, user);
+  }
+}
