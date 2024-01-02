1ce06177e82071dd47838f12a8026a407d1356af
tkyktkmt <78991629+tkyktkmt@users.noreply.github.com>
2024-01-02 17:14:24 +0900

    guards適用

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,11 +1,17 @@
 import { Query, Resolver } from '@nestjs/graphql';
+
+import { UseGuards } from '@nestjs/common';
+import { AdminGuard } from 'src/guard/admin.guard';
+import { LoggedInGuard } from 'src/guard/logged-in.guard';
+import { PrismaService } from 'src/prisma.service';
 import { UserModel } from './users.model';
-import { PrismaService } from '../prisma.service';
 
 @Resolver(() => UserModel)
 export class UsersResolver {
   constructor(private readonly prismaService: PrismaService) {}
 
+  // NOTE:ログイン済みのユーザーのみが実行できる
+  @UseGuards(LoggedInGuard)
   @Query(() => [UserModel], { name: 'users', nullable: true })
   async getUsers() {
     return this.prismaService.user.findMany({
@@ -15,6 +21,8 @@ export class UsersResolver {
     });
   }
 
+  // NOTE: Adminロールを持ったユーザーのみが実行できる
+  @UseGuards(AdminGuard)
   @Query(() => [UserModel], { name: 'allUsers', nullable: true })
   async getAllUsers() {
     return this.prismaService.user.findMany();
